b6427856c76825061b94b10d6987aff1049cae8c
Jay Collett <hello@jaycollett.co>
2023-05-04 13:08:16 +0100

    build work to fix icons

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import vercelServerless from '@astrojs/vercel/serverless';
 import vercelStatic from '@astrojs/vercel/static';
 import sanity from 'astro-sanity';
@@ -12,25 +12,23 @@ const { PUBLIC_SANITY_API_VERSION } = loadEnv(import.meta.env.PUBLIC_SANITY_API_
 const { PUBLIC_ASTRO_OUTPUT } = loadEnv(import.meta.env.PUBLIC_ASTRO_OUTPUT, process.cwd(), "")
 
 export default defineConfig({
+
   // Ports
   server: command => ({
     port: command === 'dev' ? 6000 : 4000
   }),
 
   // Plugins
-  integrations: [compress(), sanity({
-    projectId: PUBLIC_SANITY_PROJECT_ID,
-    dataset: PUBLIC_SANITY_DATASET,
-    apiVersion: PUBLIC_SANITY_API_VERSION,
-    useCdn: true
-  })],
+  integrations: [
+    // compress(),
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      apiVersion: PUBLIC_SANITY_API_VERSION,
+      useCdn: true
+    })
+  ],
   
   // Vercel
-  // output: PUBLIC_ASTRO_OUTPUT == 'server' ? 'server' : 'static',
-  // adapter: PUBLIC_ASTRO_OUTPUT == 'server' ? vercelServerless() : (vercelStatic()),
-
   output: PUBLIC_ASTRO_OUTPUT, ...(PUBLIC_ASTRO_OUTPUT === "server" && {adapter: vercelServerless()})
-
-  // output: "static",
-  // adapter: vercelStatic(),
-});
+})
